6f362302995b0d4322c18400d6007b03f7bda7c0
andeuter <108286840+andeuter@users.noreply.github.com>
2023-05-22 16:55:31 +0200

    Eigabe einer Ausgabe

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            String name = "";
 
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("-------------------\n");
+
+            Console.WriteLine("Gib deinen Namen ein ...");
+            name = Console.ReadLine();
+            Console.WriteLine("\nHallo Welt, mein Name ist " + name + ".\n");
+
+            Console.WriteLine("Programm mit 'Enter' beenden ...");
+            Console.ReadLine();
         }
     }
 }
